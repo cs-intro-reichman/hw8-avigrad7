83ce03573075642c2eff9fd87ac719097adf89f6
Avi <avigrad7@gmail.com>
2025-01-08 17:59:19 +0200

    Fixed Another bug

--- User.java
+++ User.java
@@ -53,7 +53,7 @@
     /** Makes this user follow the given name. If successful, returns true. 
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
-        name = name.substring(0, 1).toUpperCase() + name.substring(1).toUpperCase();
+        name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
         if (follows(name) || fCount == maxfCount) {
             return false;
         }
